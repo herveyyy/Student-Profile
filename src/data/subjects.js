a47217ba87f49da88aa4e6b45afb808c6474c767
herveyyy <captainhervey@gmail.com>
2023-09-14 14:59:31 +0800

    start

---
+++ src/data/subjects.js
@@ -0,0 +1,15 @@
+// students.js
+const subjects = [
+    {
+      id: 1,
+      firstName: "Vince Harley",
+      middleName: "Cabig",
+      lastName: "Mapano",
+      age: 13,
+      grade: "Grade 8",
+      section: "Fair",
+    },
+    
+  ];
+
+  export default subjects;
